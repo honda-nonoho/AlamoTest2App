70c25ee7b8ceae553512f1447739bb04cb5f2f7a
HondaNonoho <nnhhnd3012@gmail.com>
2020-06-16 17:55:24 +0900

    Initial Commit

---
+++ AlamoTest2App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlamoTest2App
+//
+//  Created by nonoho.honda on 2020/06/16.
+//  Copyright © 2020 nonoho.honda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
